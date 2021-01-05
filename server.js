c58b7df6b6014da64651dcfd8d0b8dfbb4dfe808
lkgooding <lkgooding@users.noreply.github.com>
2021-01-05 11:59:20 +0000

    edited server.js app.get

--- server.js
+++ server.js
@@ -46,8 +46,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 
