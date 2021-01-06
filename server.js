84fba6496e263bc9872d10a0ee8c04a7de335256
lkgooding <lkgooding@users.noreply.github.com>
2021-01-06 18:29:25 +0000

    changed app.use line to /static

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ if (process.env.NODE_ENV === "production") {
   app.use('*', express.static('client/build'));
 }
 
-app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
+//app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
 app.post("/", function(req, res) {
 
