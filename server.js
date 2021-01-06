3d16fd37928f6d7435e9e5c92ca8b40119f05c25
lkgooding <lkgooding@users.noreply.github.com>
2021-01-06 17:18:21 +0000

    undone breaking changes and added new app.use * line

--- server.js
+++ server.js
@@ -36,18 +36,17 @@ createNewTest();  */
 const peopleRouter = require("./routes/people");
 app.use("/people", peopleRouter);  
 
+//For Deployment
+
+if (process.env.NODE_ENV === "production") {
+  //app.use(express.static("client/build"));
+  app.use('*', express.static("client/build"));
+}
+
 app.use(express.static('build'));
 app.get('*', function (req, res) {
   res.sendFile('index.html');
 });
-
-//For Deployment
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-
 
 app.post("/", function(req, res) {
 
