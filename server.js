30b90e5da995c83848e51e73800465a6562f3bd9
lkgooding <lkgooding@users.noreply.github.com>
2021-01-06 19:41:08 +0000

    removed app.get line

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
-
 
 
 app.post("/", function(req, res) {
