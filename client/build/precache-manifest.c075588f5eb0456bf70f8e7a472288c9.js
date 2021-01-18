c044e36b7f573a2ec9089fc0f69b852312bc07fd
lkgooding <lkgooding@users.noreply.github.com>
2021-01-18 20:28:23 +0000

    removed proxy due to headers too large error

---
+++ client/build/precache-manifest.c075588f5eb0456bf70f8e7a472288c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c51ba7aeccdae79438e717a05e3576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aefeb2c99d4e333f5c7",
+    "url": "/static/css/main.62d81941.chunk.css"
+  },
+  {
+    "revision": "a209415fc951039a6adb",
+    "url": "/static/js/2.c374807b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c374807b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aefeb2c99d4e333f5c7",
+    "url": "/static/js/main.7db1f9a4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2d9efb9c801df3922cc3db0f205ca806",
+    "url": "/static/media/pc.2d9efb9c.png"
+  },
+  {
+    "revision": "94fd9e5a6a9ef7f84f8aa33b514967f9",
+    "url": "/static/media/pc.94fd9e5a.jpg"
+  },
+  {
+    "revision": "918bbb2eea471060e24443d3d6fe9c8e",
+    "url": "/static/media/pcImg.918bbb2e.jpg"
+  },
+  {
+    "revision": "286ca883cd5d6a39643388506f16d904",
+    "url": "/static/media/undraw_data_trends_b0wg.286ca883.png"
+  },
+  {
+    "revision": "714d720b6743856dba19d493361c4b24",
+    "url": "/static/media/undraw_interview_rmcf.714d720b.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
+const path = require('path')
 
 require('dotenv').config(); //environment variables
 
-//Creates express server
+//Creates express app
 const app = express(); 
 const port = process.env.PORT || 5000;
 
-//Middleware:
-app.use(cors());
-app.use(express.json()); 
+//Middleware: 
+
+app.use(cors()); 
+app.use(express.json());  
 app.use(express.urlencoded({ extended: true }))//parses json
 
 //Set up mongoose connection
@@ -25,31 +26,29 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
 
 /*
 const testSchema= new mongoose.Schema({ testData1: String, testData2: String, email: String});
-
 const Test= mongoose.model('Test', testSchema);
-
 async function createNewTest() {const test= new Test({testData1: 'new test', testData2: 'db here', email: 'testsuccess@new.com',});const result = await test.save();console.log(result);
 }
 createNewTest();  */
 
 //Routes
 
-//For Deployment
+//let root = path.join(__dirname, '..', 'build/')
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
-  app.use('*', express.static('client/build'));    }
+ if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build')); 
+  app.use('*', express.static('client/build'));    
+}
 
-//Routes
+ const peopleRouter = require("./routes/people"); 
+ app.use("/people", peopleRouter);  
+ app.post("/", function(req, res){
 
-const peopleRouter = require("./routes/people");
-
-app.post("/", function(req, res) {
-
-const personName = req.body.newPerson; //This taps into what the user types into the form field. 
+const personName = req.body.newPerson; 
+//This taps into what the user types into the form field. 
 
 const person = new personModel({
-  	firstName: personFirstname,
+    firstName: personFirstname,
     surName: personSurname,
     email: personEmail,
     query: personQuery
@@ -63,11 +62,8 @@ res.redirect("/");
 
 app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname+ "/client/build/index.html"));
-}); 
+    }); 
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+ app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
 });
-
-
