1a8d6019bb0ba9d9bd30a7e720988364ffff9c9b
lkgooding <lkgooding@users.noreply.github.com>
2021-01-19 09:52:03 +0000

    edit server.js for production

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ createNewTest();  */
 
 //For Deployment
 
-/* if (process.env.NODE_ENV === "production") {
+ if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
   app.use('*', express.static('client/build'));    } */
 
@@ -57,9 +57,9 @@ res.redirect("/");
 
 });
 
-/* app.get("*", (req, res) => {
+ app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname+ "/client/build/index.html"));
-});  */
+}); 
 
 
 app.listen(port, () => {
